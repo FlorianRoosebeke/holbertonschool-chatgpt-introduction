051765dbb7386bee5456efa11166add0aae4f07a
Florian <12160@holbertonstudents.com>
2026-01-12 09:43:06 +0100

    add command-line argument handling to factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+import sys
+
 def factorial(n):
     result = 1
     while n > 1:
@@ -5,4 +8,13 @@ def factorial(n):
         n -= 1
     return result
 
+if len(sys.argv) != 2:
+    print("Usage: ./factorial.py <nombre>")
+    sys.exit(1)
 
+n = int(sys.argv[1])
+if n < 0:
+    print("Erreur : n doit être >= 0")
+    sys.exit(1)
+
+print(factorial(n))
