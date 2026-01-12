46bb7b91aa40d40cd8c43ecd7e03460da3956534
Florian <12160@holbertonstudents.com>
2026-01-12 09:45:00 +0100

    remove command-line argument handling and shebang from factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,20 +1,6 @@
-#!/usr/bin/python3
-import sys
-
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
         n -= 1
     return result
-
-if len(sys.argv) != 2:
-    print("Usage: ./factorial.py <nombre>")
-    sys.exit(1)
-
-n = int(sys.argv[1])
-if n < 0:
-    print("Erreur : n doit être >= 0")
-    sys.exit(1)
-
-print(factorial(n))
